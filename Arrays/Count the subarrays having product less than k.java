f0cac61e910d645453488468510678997bb38087
sai kumar <saimamindlagoud@gmail.com>
2022-01-13 00:56:34 +0530

    Update Count the subarrays having product less than k.java

--- Arrays/Count the subarrays having product less than k.java
+++ Arrays/Count the subarrays having product less than k.java
@@ -2,6 +2,16 @@ Count the subarrays having product less than k
 https://practice.geeksforgeeks.org/problems/count-the-subarrays-having-product-less-than-k1708/1/#
 https://www.youtube.com/watch?v=NcD0CZLd6xM
 
+Input : 
+n = 4, k = 10
+a[] = {1, 2, 3, 4}
+Output : 
+7
+Explanation:
+The contiguous subarrays are {1}, {2}, {3}, {4} 
+{1, 2}, {1, 2, 3} and {2, 3} whose count is 7.
+
+
 class Solution {
     public int countSubArrayProductLessThanK(long a[], long n, long k)
     {
