93bf7a2bfde0f697ed49a23465ef3918106c8f8d
sai kumar <saimamindlagoud@gmail.com>
2022-01-13 00:45:34 +0530

    Create Implement Atoi.java

---
+++ Arrays/Implement Atoi.java
@@ -0,0 +1,33 @@
+Implement Atoi
+https://practice.geeksforgeeks.org/problems/implement-atoi/1#
+
+class Solution
+{
+    int atoi(String str) {
+	// Your code here
+	    String temp = "";
+	    int number = 0;
+	    int i = 0;
+	    
+	    for(i = 0; i<str.length(); i++)
+	    {
+	       int num = str.charAt(i) - '0';
+	       if(num>=0 && num<=9)
+	       {
+	           temp += num;
+	       }
+	       else if(str.charAt(i)=='-') 
+	       {
+	           temp += '-'; //If string is negative
+	       }
+	       else
+	       {
+	           return -1;
+	       }
+	    }
+
+	    number = Integer.parseInt(temp);
+	    
+	    return number;
+    }
+}
