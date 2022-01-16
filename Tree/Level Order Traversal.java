e7cc36de217e334d5bef72c9fd8fe3ed5cdf7e3c
sai kumar <saimamindlagoud@gmail.com>
2022-01-16 11:17:29 +0530

    Update Level Order Traversal.java

--- Tree/Level Order Traversal.java
+++ Tree/Level Order Traversal.java
@@ -1,5 +1,6 @@
 Tree: Level Order Traversal
 https://www.hackerrank.com/challenges/tree-level-order-traversal/problem
+//just prints 
 
 public static void levelOrder(Node root) {
         Queue<Node> dq = new LinkedList<>();
@@ -21,4 +22,42 @@ public static void levelOrder(Node root) {
             }
         }
       
+}
+
+
+102. Binary Tree Level Order Traversal
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+//retruns 2D ArrayList
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> arr = new ArrayList<>();
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        
+        if(root == null) return arr;
+        
+        dq.add(root);
+        while(!dq.isEmpty())
+        {
+            ArrayList<Integer> subList = new ArrayList<>();
+            int n = dq.size();
+            
+            for(int i = 0; i<n; i++)
+            {
+                TreeNode node = dq.poll();
+                if(node.left!=null)
+                {
+                    dq.add(node.left);
+                }
+                if(node.right!=null)
+                {
+                    dq.add(node.right);
+                }
+                subList.add(node.val);
+            }
+            arr.add(subList);
+        }
+        
+        return arr;
+    }
 }
