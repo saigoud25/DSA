ffe3a82e9637cc52fccef2df7c1d6abfb806ef85
sai kumar <saimamindlagoud@gmail.com>
2022-03-16 19:36:34 +0530

    Create Subsets.java

---
+++ Arrays/Recursion/Subsets.java
@@ -0,0 +1,47 @@
+Subsets
+https://practice.geeksforgeeks.org/problems/subsets-1613027340/1#
+
+/*
+->Regular Subsets method, Using take or not take an element
+->Sort the result 2D ArrayList using Comparator
+*/
+class Solution
+{
+    
+    public static ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> A)
+    {
+        //code here
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        helper(arr, A, list, 0);
+        Collections.sort(arr, new Sort());
+        return arr;
+    }
+    public static void helper(ArrayList<ArrayList<Integer>> arr, ArrayList<Integer> A, ArrayList<Integer> list, int i)
+    {
+        if(i==A.size())
+        {
+            arr.add(new ArrayList<>(list));
+            return;
+        }
+        list.add(A.get(i));
+        helper(arr, A, list, i+1);
+        list.remove(list.size()-1);
+        helper(arr, A, list, i+1);
+
+        return;
+    }
+    static class Sort implements Comparator<ArrayList<Integer>>
+    {
+        public int compare(ArrayList<Integer> a, ArrayList<Integer> b)
+        {
+            for(int i = 0; i<Math.min(a.size(), b.size()); i++)
+            {
+                if(a.get(i)>b.get(i)) return 1;
+                else if(a.get(i)<b.get(i)) return -1;
+                else continue;
+            }
+            return a.size()-b.size();
+        }
+    }
+}
