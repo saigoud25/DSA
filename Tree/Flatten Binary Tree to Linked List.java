9f35ee8412a044a1e658284bc1bd8282761c58b8
sai kumar <saimamindlagoud@gmail.com>
2022-07-28 02:05:00 +0530

    Update Flatten Binary Tree to Linked List.java

--- Tree/Flatten Binary Tree to Linked List.java
+++ Tree/Flatten Binary Tree to Linked List.java
@@ -1,6 +1,31 @@
 114. Flatten Binary Tree to Linked List
 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 
+Method-1
+/*
+->Traverse right sub tree first and then left sub tree, 
+->Make root.right as prev, root.left as null ans store the prev = root
+*/
+class Solution {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        
+        flatten(root.right);
+        flatten(root.left);
+        
+        root.right = prev;
+        root.left = null;
+        
+        prev = root;
+    }
+}
+
+
+
+Method-2
 /*
 1.Preoder(l-R-r) traverse the tree and store them in a array
 2.iterate throgh the arays and make root.right=array(i), root.null=null, update root=root.right
