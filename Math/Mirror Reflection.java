bdbbbb31533eb6006f3c4f460378537e44bbc7eb
sai kumar <saimamindlagoud@gmail.com>
2022-08-04 23:10:18 +0530

    Create Mirror Reflection.java

---
+++ Math/Mirror Reflection.java
@@ -0,0 +1,22 @@
+858. Mirror Reflection
+https://leetcode.com/problems/mirror-reflection/
+
+
+/*
+https://youtu.be/uCULGjx6HHk
+
+*/
+class Solution {
+    public int mirrorReflection(int p, int q) {
+        while(p%2==0 && q%2==0) {
+            p /= 2;
+            q /= 2;
+        }
+        
+        if(p%2 == 0 && q%2 == 1) return 2;
+        
+        if(p%2 != 0 && q%2 == 0) return 0;
+        
+        return 1;
+    }
+}
