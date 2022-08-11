e303c7d825b9cfccea9e561aa1500ca14057ca42
sai kumar <saimamindlagoud@gmail.com>
2022-08-12 01:09:32 +0530

    Update Validate Binary Search Tree.java

--- Tree/Validate Binary Search Tree.java
+++ Tree/Validate Binary Search Tree.java
@@ -2,6 +2,34 @@
 https://leetcode.com/problems/validate-binary-search-tree/
 50%
 
+/*
+Method-1
+->Inorder Traverse the Tree and store the elements in the ArrayList
+->If the elements in the arraylist are not sorted order then it's not a valid BST
+
+Method-2
+->Maintain two values min and max
+->Check whether the root.val is between two values, else return false
+->While calling the left/right trees update the min/max with root.val
+
+*/
+//Method-1
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+        return helper(Long.MIN_VALUE, Long.MAX_VALUE, root);
+    }
+    public boolean helper(long min, long max, TreeNode root) {
+        if(root==null) return true;
+        
+        if((min >= root.val) || (max <= root.val)) return false;
+        
+        return helper(min, root.val, root.left) && helper(root.val, max, root.right);
+    }
+} 
+    
+    
+//Method-2   
 class Solution {
     public boolean isValidBST(TreeNode root) {
         
