d1f356fc1be205a8a048e171d89e39ce887526cd
sai kumar <saimamindlagoud@gmail.com>
2022-10-04 16:29:53 +0530

    Create Path Sum.java

---
+++ Tree/Path Sum.java
@@ -0,0 +1,19 @@
+112. Path Sum
+https://leetcode.com/problems/path-sum/
+
+/*
+-> Traverse the Tree, by checking targetSum
+*/
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        
+        if(root.left==null && root.right==null) {
+            if(targetSum-root.val == 0) {
+                return true;
+            }
+        }
+        
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
+    }
+}
