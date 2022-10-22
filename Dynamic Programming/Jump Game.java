95cd74b29d878dae9e2528d294ed59a7ae6dcab0
sai kumar <saimamindlagoud@gmail.com>
2022-10-22 23:49:23 +0530

    Update Jump Game.java

--- Dynamic Programming/Jump Game.java
+++ Dynamic Programming/Jump Game.java
@@ -1,4 +1,83 @@
 Jump Game
+https://leetcode.com/problems/jump-game/
+
+/*
+->Method-1 
+->Iterate from the back, 
+->If 0 appeared, find the whether there is an required number before it's  index
+
+->Regular DP (Recursion + Memoization)
+*/
+class Solution {
+    int dp[];
+    public boolean canJump(int[] nums) {
+        //Method-1
+        int n = nums.length;
+        
+        if(n < 2) return true;
+        
+        if(nums[0]==0) return false;
+        
+        for(int i = n-2; i>0; i--) {
+            if(nums[i] == 0) {
+                int req = 2;
+                
+                while(req > nums[i-1]) {
+                    req++;
+                    i--;
+                    
+                    if(i==0) {
+                        return false;
+                    }
+                }
+            }
+        } 
+        
+        
+        return true;
+        
+//        Method-2 Recursion + Memoization(DP)
+//         if(nums.length==1) return true;
+        
+//         dp = new int[nums.length];
+        
+//         boolean ans = helper(0, nums);
+//         return ans;
+    }
+    
+    public boolean helper(int i, int[] nums) {
+        if(i>=nums.length-1) {
+            return true;
+        }
+        
+        if(nums[i]==0) {
+            return false;
+        }
+        
+        if(dp[i] != 0) {
+            return dp[i]==2 ? true : false;
+        }
+        
+        boolean ans = false;
+        
+        for(int k = i+1; k<=i+nums[i]; k++) {
+            ans = helper(k, nums);
+            
+            if(ans) {
+                break;
+            }
+        }
+        
+        dp[i] = ans ? 2 : 1;
+        
+        return ans;
+    }
+}
+
+
+
+
+Jump Game
 https://practice.geeksforgeeks.org/problems/jump-game/1/#
 
 /*
